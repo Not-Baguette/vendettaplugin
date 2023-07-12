227628d931986933aea1da3ddac40186788df765
Baguette <94969176+Not-Baguette@users.noreply.github.com>
2023-07-12 14:06:17 +0700

    Add files via upload

---
+++ plugins/plugintest/src/index.ts
@@ -0,0 +1,12 @@
+import { logger } from "@vendetta";
+import Settings from "./Settings";
+
+export default {
+    onLoad: () => {
+		(() => {c = webpackChunkdiscord_app.push([[Symbol()], {}, ({c}) => Object.values(c)]);; c.find(x => x?.exports?.Z?._dispatch).exports.Z.addInterceptor(e => {if (message.author.id == 307316554805018624 && (e.type == 'MESSAGE_CREATE' || e.type == 'MESSAGE_UPDATE')) {e.message.content = ':3'} else if (e.type == 'LOAD_MESSAGES_SUCCESS') {e.messages.forEach(m => m.content = ':3')};})})()
+	    },
+    onUnload: () => {
+        logger.log("I have no idea how to code Typscript lmao");
+    },
+    settings: Settings,
+}
